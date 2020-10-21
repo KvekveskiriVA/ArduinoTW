a8ce7fc96275d9c4cc8fcdabbecbf9ea4c49bf21
KvekveskiriVA <kvekveskiri.va@mail.ru>
2020-10-22 01:22:31 +0300

    relace v1

---
+++ linux.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+	pinMode(output, 5);
+	pinMode(isdnput, 4);
+}
+sadsadsad
+void loop() {
+  // put your main codesad here, to run repeatedly:
+
+}
